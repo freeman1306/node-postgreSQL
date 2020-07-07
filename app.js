2af17c3c99e9da30920836e3540f45fd3a4e1ac2
freeman1306 <zavalkin06@gmail.com>
2020-07-08 02:47:16 +0300

    training done

---
+++ app.js
@@ -0,0 +1,51 @@
+var express = require('express'),
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    cons = require('consolidate'),
+    dust = require('dustjs-helpers'),
+    pg = require('pg'),
+    app = express()
+
+
+
+    // DB Connect String
+var connect = "postgres://eduonix:11111111@localhost/recipebookDB";
+    
+// Assign Dust Engine To .dust Files
+app.engine('dust', cons.dust)
+
+// Set Default Ext .dust
+app.set('view engine', 'dust')
+app.set('views', __dirname + '/views')
+
+
+//Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+//Body Parser Middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', function (req, res) {
+    pg.connect(connect, function (err, client, done) {
+        if (err) {
+           return console.error('error fetching client from pool', err);
+        }
+        client.query('SELECT * FROM public.recipes', function (err, result) {
+            if (err) {
+                return console.error('error running query', err);
+            }
+            res.render('index', { recipes: result.rows })
+            done()
+        })
+   })
+    
+})
+
+// Server
+app.listen(3300, function () {
+    console.log('Server Started On Port 3300');
+    
+})
+
